9c4c5c0e3ec4d546575cdc6941102bc550b54b19
Bruno Torrinha <bruno@torrinha.com>
2018-10-11 09:52:39 +0100

    Clear AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,12 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Schema::defaultStringLength(191);
-
-        // Force SSL in production
-        // if ($this->app->environment() == 'production') {
-        //     URL::forceScheme('https');
-        // }
+        //
     }
 
     /**
